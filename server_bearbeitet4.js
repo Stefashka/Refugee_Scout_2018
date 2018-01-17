8bc12381f50496e2b8e6bed54b6050ff06c1013a
Gockel4Life <giftiger_gockel@web.de>
2018-01-17 18:29:21 +0100

    Muuuuh!

--- server_bearbeitet4.js
+++ server_bearbeitet4.js
@@ -25,10 +25,135 @@ var connection = mysql.createConnection({
 /** Bodyparser wird verwendet? */
 app.use(myParser.urlencoded({extended : true}));
 
+//Session Versuch
+
+app.use(session({secret: 'ssshhhhh'}));
+
+var sess;
+app.get('/',function(req,res){
+    sess=req.session;
+
+    sess.email;
+    sess.username;
+
+});
+
+// const express = require('express');
+const fileUpload = require('express-fileupload');
+// const app = express();
+
+//TEst Bildupload
+
+app.get("/upload", function(request, response) {
+    console.log("upload");
+    response.sendFile('./testindex.html');
+});
+
+app.use(fileUpload());
+
+app.post('/upload', function(req, res) {
+    if (!req.files)
+        return res.status(400).send('No files were uploaded.');
+
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    let sampleFile = req.files.sampleFile;
+
+    // Use the mv() method to place the file somewhere on your server
+    sampleFile.mv('./storyimages/filename.jpg', function(err) {
+        if (err)
+            return res.status(500).send(err);
+
+        res.send('File uploaded!');
+    });
+});
+// Ende Test Bildupload
+
+
+
+
+//TESTTTTT!!!++
+
+/*
+
+//var express = require('express');
+//var session = require('express-session');
+//var bodyParser = require('body-parser');
+//var app = express();
+
+app.set('views', __dirname + '/views');
+
+app.engine('html', require('ejs').renderFile);
+
+app.use(session({secret: 'ssshhhhh'}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var sess;
+
+app.get('/',function(req,res){
+    sess = req.session;
+//Session set when user Request our app via URL
+    if(sess.email) {
+        /*
+        * This line check Session existence.
+        * If it existed will do some action.
+        */
+/*        res.redirect('/admin');
+    }
+    else {
+        res.render('index.html');
+    }
+});
+
+app.post('/login',function(req,res){
+    sess = req.session;
+//In this we are assigning email to sess.email variable.
+//email comes from HTML page.
+    sess.email=req.body.email;
+    res.end('done');
+});
+
+app.get('/admin',function(req,res){
+    sess = req.session;
+    if(sess.email) {
+        res.write('
+            <h1>Hello '+sess.email+'</h1>
+        ');
+        res.end('<a href="+">Logout</a>');
+    } else {
+        res.write('
+            <h1>Please login first.</h1>
+        ');
+        res.end('<a href="+">Login</a>');
+    }
+});
+
+app.get('/logout',function(req,res){
+    req.session.destroy(function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect('/');
+        }
+    });
+
+});
+app.listen(3000,function(){
+    console.log("App Started on PORT 3000");
+});
+
+*/
+//ENDE TEST!!!!
+
+
+
+
+
+
 /** Abruf von login.html, webb /login aufgerufen wird */
 app.get("/login", function(request, response) {
 	console.log("login");
-	response.sendFile('C:/Medienprojekt/refugee_authoring_tool/index2.html');
+	response.sendFile('./index.html');
 });
 
 
@@ -108,7 +233,7 @@ app.post("/loginuser", function(request, response) {
 
       if(results[0].password === password){
 
-          response.sendFile('C:/Medienprojekt/refugee_authoring_tool/myprofile.html');
+          response.sendFile('./myprofile.html');
 
 
        // response.send({
