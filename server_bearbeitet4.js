cbc3afc6f00ec48ab1e1325dffd3fd9f4e41139c
Gockel4Life <giftiger_gockel@web.de>
2018-01-22 17:23:02 +0100

    Session weiter versucht

--- server_bearbeitet4.js
+++ server_bearbeitet4.js
@@ -12,7 +12,6 @@ var app = express();
 var mysql = require('mysql');
 
 
-
 var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -27,22 +26,33 @@ app.use(myParser.urlencoded({extended : true}));
 
 //Session Versuch
 
-app.use(session({secret: 'ssshhhhh'}));
+console.log ('Session Stelle 1 wird gestartet');
+
+app.use(session({
+
+    secret: 'ssshhhhh',
+    // name: cookie_name,
+    // store: sessionStore,
+    proxy: true,
+    resave: true,
+    saveUninitialized: true
+
+}));
+
+console.log (session);
 
 var sess;
-app.get('/',function(req,res){
-    sess=req.session;
 
-    sess.email;
-    sess.username;
+// app.get('/',function(req,res){
+//    sess=req.session;
 
-});
+//    sess.email;
 
-// const express = require('express');
-const fileUpload = require('express-fileupload');
-// const app = express();
+// });
 
 //TEst Bildupload
+
+const fileUpload = require('express-fileupload');
 
 app.get("/upload", function(request, response) {
     console.log("upload");
@@ -68,94 +78,11 @@ app.post('/upload', function(req, res) {
 });
 // Ende Test Bildupload
 
-
-
-
-//TESTTTTT!!!++
-
-/*
-
-//var express = require('express');
-//var session = require('express-session');
-//var bodyParser = require('body-parser');
-//var app = express();
-
-app.set('views', __dirname + '/views');
-
-app.engine('html', require('ejs').renderFile);
-
-app.use(session({secret: 'ssshhhhh'}));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-var sess;
-
-app.get('/',function(req,res){
-    sess = req.session;
-//Session set when user Request our app via URL
-    if(sess.email) {
-        /*
-        * This line check Session existence.
-        * If it existed will do some action.
-        */
-/*        res.redirect('/admin');
-    }
-    else {
-        res.render('index.html');
-    }
-});
-
-app.post('/login',function(req,res){
-    sess = req.session;
-//In this we are assigning email to sess.email variable.
-//email comes from HTML page.
-    sess.email=req.body.email;
-    res.end('done');
-});
-
-app.get('/admin',function(req,res){
-    sess = req.session;
-    if(sess.email) {
-        res.write('
-            <h1>Hello '+sess.email+'</h1>
-        ');
-        res.end('<a href="+">Logout</a>');
-    } else {
-        res.write('
-            <h1>Please login first.</h1>
-        ');
-        res.end('<a href="+">Login</a>');
-    }
-});
-
-app.get('/logout',function(req,res){
-    req.session.destroy(function(err) {
-        if(err) {
-            console.log(err);
-        } else {
-            res.redirect('/');
-        }
-    });
-
-});
-app.listen(3000,function(){
-    console.log("App Started on PORT 3000");
-});
-
-*/
-//ENDE TEST!!!!
-
-
-
-
-
-
 /** Abruf von login.html, webb /login aufgerufen wird */
 app.get("/login", function(request, response) {
 	console.log("login");
 	response.sendFile('C:/users/elisabeth/Refugee_Scout_2018/index.html');
 });
-
 
 /** Zurückschicken der eingegebenen Daten vom Browser zum Server (Bei KLick auf register) */
 // Nutzereingabe in 2 Variablen überführen: BENUTZERNAME & PASSWORT
@@ -165,45 +92,38 @@ app.get("/login", function(request, response) {
 
 //registration
 app.post("/registeruser", function(request, response) {
-//oder exports.register = function(req,res){ ??
-    console.log(request.body); //This prints the JSON document received (if it is a JSON document)
-		//oder console.log("req",req.body);
-	  // var today = new Date();
 
+    console.log(request.body); //This prints the JSON document received (if it is a JSON document)
 
 	  var users= {
 	    "username":request.body.username,
 	    "email":request.body.email,
 	    "password":request.body.password,
-	   // "created":today,
-	   // "modified":today
-	  } //Strichpunkt?
+	  }
 
 	  connection.query('INSERT INTO author SET ?',users, function (error, results, fields) { //wo ist fields?
+
 	  	if (error) {
 	    	console.log("error ocurred",error);
 	    	response.send({
 	      	"code":400,
 					"failed":"error ocurred"
-    		}) //Strichpunkt?
-			} //Strichpunkt?
+    		})
+			}
+
 			else {
     	console.log('The solution is: ', results);
     		response.send({
       	"code":200,
       	"success":"user registered sucessfully"
-                //window.location='newUrl.html';
-                //oder window.location.href="";
-                //oder res.redirect ('/userpage');
         });
-  		} //Strichpunkt?
-  	}); //Zu was gehört die normale Klammerzu?
+  		}
+  	});
 });
 
 //login abgleichen
 
 app.post("/loginuser", function(request, response) {
-// oder exports.login = function(req,res){ ??
 
   var username = request.body.username;
   var password = request.body.password;
@@ -225,15 +145,94 @@ app.post("/loginuser", function(request, response) {
 
     if(results.length > 0){
 
-         // HIER BESTEHT UNKLARHEIT WARUM DIE PASSWÖRTER  NICHT ÜBEREINSTIMMEN!!!
-
         console.log(results[0].password);
         console.log(password);
 
+        // Hier Session einfügen
+
+        var sess = request.session;
+
+        sess.email = username;
+
+        console.log("Session wird angelegt:");
+        console.log(sess);
+
+        // More Bullshit
+
+        //app.set('views', __dirname + '/views');
+
+        //app.engine('html', require('ejs').renderFile);
+
+        app.use(session({
+            secret: 'ssshhhhh',
+           // name: cookie_name,
+           // store: sessionStore,
+            proxy: true,
+            resave: true,
+            saveUninitialized: true
+
+        }));
+       // app.use(bodyParser.json());
+       // app.use(bodyParser.urlencoded({extended: true}));
+
+        app.get('/',function(request,response){
+            sess = request.session;
+
+        //Session set when user Request our app via URL
+            if(sess.email) {
+                /*
+                * This line check Session existence.
+                * If it existed will do some action.
+                */
+                res.redirect('/admin');
+            }
+            else {
+                res.render('index.html');
+            }
+        });
+
+        app.post('/login',function(request,response){
+            sess = request.session;
+
+            //In this we are assigning email to sess.email variable.
+            //email comes from HTML page.
+
+            sess.email=request.body.email;
+            response.end('done');
+        });
+
+        app.get('/admin',function(request,response){
+            sess = request.session;
+            if(sess.email) {
+                response.write('<h1>Hello '+sess.email+'</h1>');
+                response.end('<a href="+">Logout</a>');
+            } else {
+                response.write('<h1>Please login first.</h1>');
+                response.end('<a href="+">Login</a>');
+            }
+        });
+
+        app.get('/logout',function(request,response){
+            request.session.destroy(function(err) {
+                if(err) {
+                    console.log(err);
+                } else {
+                    response.redirect('/');
+                }
+            });
+
+        });
+
+        // app.listen(3000,function(){
+        // console.log("App Started on PORT 3000");
+        // });
+
+        // End Bullshit
 
       if(results[0].password === password){
 
           response.sendFile('C:/users/elisabeth/Refugee_Scout_2018/myprofile.html');
+
 
 
        // response.send({
